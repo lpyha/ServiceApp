a6d63774b3d9e2ee9e00efe7ef97f1a9a026c8d5
murakami <halpygg@gmail.com>
2023-05-09 11:41:07 +0900

    ReceiverからUIの更新

--- app/src/main/java/com/example/serviceapp/MainActivity.java
+++ app/src/main/java/com/example/serviceapp/MainActivity.java
@@ -1,12 +1,20 @@
 package com.example.serviceapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.ButtonBarLayout;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.widget.Button;
 import android.widget.TextView;
+
+
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,20 +26,47 @@ public class MainActivity extends AppCompatActivity {
 
         Button buttonBegin = (Button) findViewById(R.id.button_begin);
         Button buttonEnd = (Button) findViewById(R.id.button_end);
-        Intent intent = new Intent(this, SimpleService.class);
 
-        MyReceiver receiver = new MyReceiver();
+
+
         IntentFilter intentFilter = new IntentFilter();
         intentFilter.addAction(SimpleService.ACTION);
+
+        // Receiverの登録、manifestにも追記する必要があるReceiverも存在する
+        registerReceiver(mReceiver, intentFilter);
+        MyReceiver receiver = new MyReceiver();
         registerReceiver(receiver, intentFilter);
 
+
+        Intent intent = new Intent(this, SimpleService.class);
         buttonBegin.setOnClickListener(view -> {
+            // serviceの起動
             startService(intent);
         });
 
         buttonEnd.setOnClickListener(view -> {
+            // serviceの終了
             stopService(intent);
         });
-
     }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        unregisterReceiver(mReceiver);
+    }
+
+    // UIの更新のためにReceiverをMainActivityに記述する
+    BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            TextView textView = (TextView) findViewById(R.id.textView);
+            String msg = intent.getStringExtra("message");
+            textView.setText(msg);
+        }
+    };
+
+
+
+
 }
--- app/src/main/java/com/example/serviceapp/MyReceiver.java
+++ app/src/main/java/com/example/serviceapp/MyReceiver.java
@@ -3,6 +3,9 @@ package com.example.serviceapp;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -12,9 +15,11 @@ public class MyReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         // TODO: This method is called when the BroadcastReceiver is receiving
         // an Intent broadcast.
-
         String msg = intent.getStringExtra("message");
         Toast.makeText(context, msg, Toast.LENGTH_LONG).show();
+
+
         //throw new UnsupportedOperationException("Not yet implemented");
     }
+
 }
